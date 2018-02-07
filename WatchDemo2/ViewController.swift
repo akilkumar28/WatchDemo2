d2827e7a4c75661009b43e5252e526cfc5bc6c84
akilkumar28 <akil.psn@gmail.com>
2018-02-06 19:40:21 -0700

    Initial Commit

---
+++ WatchDemo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchDemo2
+//
+//  Created by AKIL KUMAR THOTA on 2/6/18.
+//  Copyright © 2018 AKIL KUMAR THOTA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
