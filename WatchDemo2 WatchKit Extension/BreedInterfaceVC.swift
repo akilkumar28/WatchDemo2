95fbe8bd713194c4d025a3d15f771e6520f86bea
akilkumar28 <akil.psn@gmail.com>
2018-02-06 20:24:46 -0700

    dogInfoUIBuilt
    interface for dog infor built

---
+++ WatchDemo2 WatchKit Extension/BreedInterfaceVC.swift
@@ -0,0 +1,14 @@
+//
+//  BreedInterfaceVC.swift
+//  WatchDemo2 WatchKit Extension
+//
+//  Created by AKIL KUMAR THOTA on 2/6/18.
+//  Copyright © 2018 AKIL KUMAR THOTA. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class BreedInterfaceVC: WKInterfaceController {
+
+}
---
+++ WatchDemo2 WatchKit Extension/IndexRowType.swift
@@ -0,0 +1,14 @@
+//
+//  IndexRowType.swift
+//  WatchDemo2 WatchKit Extension
+//
+//  Created by AKIL KUMAR THOTA on 2/6/18.
+//  Copyright © 2018 AKIL KUMAR THOTA. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class IndexRowType: NSObject {
+
+}
